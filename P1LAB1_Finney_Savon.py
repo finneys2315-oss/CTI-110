cfd53a50d2bc1f57af2592047f59dbf570036938
finneys2315-oss <finneys2315@student.faytechcc.edu>
2026-02-05 10:35:27 -0500

    Add files via upload

---
+++ P1LAB1_Finney_Savon.py
@@ -0,0 +1,13 @@
+# Savon Finney
+
+# 02/05/2026
+
+# P1LAB1
+
+# The goal for this Lab to get input from the user and show the output of the results.
+
+
+User_first_name = input("Type your first name here: ")
+User_last_name = input("Type your last name here: ")
+
+print(f"Hello, {User_first_name} {User_last_name}! Welcome to CTI-110 and I hope you enjoy your time here!")
