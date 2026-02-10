c632e30957b65f71acf8223d4e30f3f2ff0cdfe6
finneys2315-oss <finneys2315@student.faytechcc.edu>
2026-02-10 11:06:50 -0500

    Add files via upload

---
+++ P1HW1_FinneySavon.py
@@ -0,0 +1,46 @@
+
+# Savon Finney
+
+# 2/10/2026
+
+# P1HW1
+
+# Learning how to collect information and process data in Python
+
+
+# --- Calculating Exponents ---
+
+Value1 = int(input("What is the number this time?: "))
+
+Value2 =  int(input("And the second number?: "))
+
+Result = Value1** Value2
+
+# Display the Results of our Exponents
+
+print()
+print(f"{Value1} raised to the power of {Value2} is {Result} !!")
+print()
+print(type(Value1))
+print(type(Value2))
+
+
+# --- Addition and Subtraction ---
+
+print()
+
+
+Value3 = int(input("Addition now: "))
+Value4 = int(input("And the second integer: "))
+Value5 = int(input("Surprise now we want a third number: "))
+
+Result2 = Value3 + Value4 - Value5
+
+
+print()
+print(f"{Value3} + {Value4} - {Value5} is equal to {Result2}")
+
+print()
+print(type(Value3))
+print(type(Value4))
+print(type(Value5))
