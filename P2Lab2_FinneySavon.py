0d0fe2ab34ca750d267b00c3671998e78b025b53
finneys2315-oss <finneys2315@student.faytechcc.edu>
2026-02-19 11:47:56 -0500

    Add files via upload

---
+++ P2Lab2_FinneySavon.py
@@ -0,0 +1,22 @@
+# Savon Finney
+
+# 02/19/2026
+
+# P2Lab2
+
+# dictionary
+
+family_of_values = {'Camro':18.21, 'Prius':52.36, "Model S":110, "Silverado":26}
+
+car_keys = family_of_values()
+
+print(f"Here is our list of cars:{car_keys}")
+
+choose_your_car = input("Which car you want to ride?: ")
+
+print(f"{choose_your_car} has {choose_your_car[family_of_values]}")
+
+Miles = float(input("How manys miles you want to drive {choose_your_car}?: "))
+
+
+Gallons = 
