0f7aa33467ff4478ce02727fcef0ab51f90bf4e2
finneys2315-oss <finneys2315@student.faytechcc.edu>
2026-02-21 10:17:02 -0500

    Add files via upload

--- P2Lab2_FinneySavon.py
+++ P2Lab2_FinneySavon.py
@@ -4,19 +4,28 @@
 
 # P2Lab2
 
-# dictionary
+# Learning how to use dictionary in Python.
 
 family_of_values = {'Camro':18.21, 'Prius':52.36, "Model S":110, "Silverado":26}
 
-car_keys = family_of_values()
+car_keys = family_of_values.keys()
 
 print(f"Here is our list of cars:{car_keys}")
+print()
+
 
 choose_your_car = input("Which car you want to ride?: ")
+print()
 
-print(f"{choose_your_car} has {choose_your_car[family_of_values]}")
+print(f"{choose_your_car} has {family_of_values[choose_your_car]}")
 
-Miles = float(input("How manys miles you want to drive {choose_your_car}?: "))
+print()
+
+Miles = float(input(f"How manys miles you want to drive {choose_your_car}?: "))
+print()
 
 
-Gallons = 
+Gallons = Miles * 0.02
+
+print(f"{Gallons} gallons of gas are needed to drive {choose_your_car} {Miles} miles.")
+
