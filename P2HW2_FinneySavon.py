0c0b453df6a2cbc932ef9dfae8e53d21f2dfd664
finneys2315-oss <finneys2315@student.faytechcc.edu>
2026-03-01 21:26:47 -0500

    Add files via upload

--- P2HW2_FinneySavon.py
+++ P2HW2_FinneySavon.py
@@ -19,20 +19,29 @@ Module_4 = float(input("What is your score for Module 4?: "))
 Module_5 = float(input("What is your score for Module 5?: "))
 Module_6 = float(input("What is your score for Module 6?: "))
 
+
 # Storing the user scores in a list
 
 Grades_for_our_user = [Module_1, Module_2, Module_3, Module_4, Module_5, Module_6]
+
+Sum_of_grades = math.fsum(Grades_for_our_user)
+
+Lowest_grade = min(Grades_for_our_user)
+Highest_grade = max(Grades_for_our_user)
+Average = Sum_of_grades / 6
 
 
 # Displaying the grade results from our user's scores.
 
 
-print(f"------Results------")
-print()
+print(f"------Results------------")
+print(f"Lowest Grade: {Lowest_grade:>11}")
+print(f"Highest Grade: {Highest_grade:>10}")
+print(f"Sum of Grades: {Sum_of_grades:>10}")
+print(f"Average: {Average:>17.2f}  ")
+print("-----------------------------")
 
-print(f"Lowest Grade: {min(Grades_for_our_user)}")
-print(f"Highest Grade: {max(Grades_for_our_user)}")
-print(f"Sum of Grades: {math.fsum(Grades_for_our_user)}")
+
 
 
 
