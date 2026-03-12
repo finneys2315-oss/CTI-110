d9ba042a49343edd9acffe44d9debc9fc8abab84
finneys2315-oss <finneys2315@student.faytechcc.edu>
2026-03-12 10:25:03 -0400

    Add files via upload

--- P3HW1_FinneySavon.py
+++ P3HW1_FinneySavon.py
@@ -14,18 +14,18 @@ mod_1 = float(input('Enter grade for Module 1: '))
 mod_2 = float(input('Enter grade for Module 2: '))
 mod_3 = float(input('Enter grade for Module 3: '))
 mod_4 = float(input('Enter grade for Module 4: '))
-mod_1 = float(input('Enter grade for Module 5: '))
-mod_5 = float(input('Enter grade for Module 6: '))
+mod_5 = float(input('Enter grade for Module 5: '))
+mod_6 = float(input('Enter grade for Module 6: '))
 
 # add grades entered to a list
 
-grades = [mod_1, mod_2, mod_3, mod_4, mod_5]
+grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
 # TO DO: determine lowest, highest , sum and average for grades
 
 low = min(grades)
 high = max(grades)
 sum = math.fsum(grades)
-avg = high / 5
+avg = sum / 6
 
 # determine letter grade for average
 
@@ -34,10 +34,10 @@ avg = high / 5
 
 print('---------------Results-----------')
 
-print(f"Lowest Grade: {low}")
-print(f"Highest Grade: {high}")
-print(f"Sum of Grades: {sum}")
-print(f"Average: {avg}")
+print(f"Lowest Grade: {low:>18}")
+print(f"Highest Grade: {high:>17}")
+print(f"Sum of Grades: {sum:>17}")
+print(f"Average: {avg:>23.1f}")
 
 print(f"-----------------------------------")
 
@@ -46,14 +46,15 @@ if avg >= 90:
 else:
     if avg >= 80:
         print('Your grade is: B')
-if avg >= 70:
-    print('Your grade is: C')
-else:
-    if avg >= 50:
-        print('Your grade is: D')
     else:
-        if avg >= 30:
-            print('Your grade is F')
+        if avg >= 70:
+            print('Your grade is: C')
+        else:
+            if avg >= 50:
+                print('Your grade is: D')
+            else:
+                if avg <= 30:
+                    print('Your grade: is F')
 
 
 
