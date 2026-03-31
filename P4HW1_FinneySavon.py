97cd9757ab213d9ac98ffc4a43e5bb5a6b7a6deb
finneys2315-oss <finneys2315@student.faytechcc.edu>
2026-03-31 10:52:10 -0400

    Add files via upload

--- P4HW1_FinneySavon.py
+++ P4HW1_FinneySavon.py
@@ -1,4 +1,4 @@
-# Savon Finney 
+# Savon Finney
 
 # 3/24/2026
 
@@ -15,8 +15,6 @@ import math
 user_score = int(input("What is the amount of scores you want to enter?: "))
 
 
-
-
 vaild_score_list = []
 
 vaild_score = 1
@@ -27,22 +25,25 @@ Done = ""
 while Done not in ["Yes", "Y","yes", "y" "Done", "Finish"]:
     for x in range(user_score):
         
-        score = float(input(f"Enter score {vaild_score}: "))
+        score = float(input(f"Enter score #{vaild_score}: "))
         
         if -1 < score < 101:
             vaild_score_list.append(score)
             vaild_score = vaild_score + 1
         else:
-            print("INVAILD SCORE ENTERED!!!!!!!!!!!!, score should be between 0 and 100")
-            score = float(input(f"Enter score {vaild_score} again: "))
+            while score < 0 or score > 100:
+                print("INVAILD SCORE ENTERED!!!!!!!!!!!!, score should be between 0 and 100")
+                score = float(input(f"Enter score #{vaild_score} again: "))
             vaild_score = vaild_score + 1
-    Done = input("Are you done?: ")        
+            vaild_score_list.append(score)
+    Done = input("Are you done?: ")
 
 Low = min(vaild_score_list)
 High = max(vaild_score_list)
 sum = math.fsum(vaild_score_list)
 average = sum / user_score
 Grade = ""
+
 if average == 100:
     Grade = "A+"
 else:
@@ -64,12 +65,12 @@ else:
 
 
 
-
-# Display our the score our user inputted
+# Display our the scores our user inputted
 
 print("---------------Results-------------------")
 
 print(f"{'Lowest Score'} : {Low:>6}")
+vaild_score_list.remove(Low)
 print(f"{'Modified List'} : {vaild_score_list}")
 print(f"{'Scores Average'} : {average:>5.2f}")
 print(f"{'Grade'}      : {Grade:>5}")
